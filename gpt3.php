a655caf30c3dfbd3001ac785b9dc6f73fbeac76e
Enes BİBER <lazenes@gmail.com>
2021-12-18 01:25:09 +0300

    Örnek Çeviri  Scripti
    PHP Scripti Kullanım: https://site.com/gpt3.php?metin=Hello World

---
+++ gpt3.php
@@ -0,0 +1,36 @@
+<?php
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://api.openai.com/v1/engines/davinci/completions',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS =>'{
+  
+  "prompt": "English:'.$_GET['metin'].'}",
+  "temperature": 0.5,
+  "max_tokens": 100,
+  "top_p": 1.0,
+  "frequency_penalty": 0.0,
+  "presence_penalty": 0.0,
+  "stop": "[\n]"
+}',
+  CURLOPT_HTTPHEADER => array(
+    'Content-Type: application/json',
+    'Authorization: Bearer APİ_KODUNUZ'
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+header('Content-Type: application/json');
+$array=json_decode($response,true);
+echo json_encode($array,JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+ 
+?>
